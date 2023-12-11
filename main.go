32360fb73b5d6684d866afe467945b2eb5fdf714
Christian Gitter <christian.gitter@lhind.dlh.de>
2023-12-11 12:10:35 +0100

    add db connection

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"GOrepos/GoAPI/controller"
+	"GOrepos/GoAPI/model"
+	"log"
 	"net/http"
 )
 
 func main() {
 	mux := controller.Register()
-	http.ListenAndServe(":3000", mux)
+	db := model.Connect()
+	defer db.Close()
+
+	// log.Fatal is used to log errors and exit the program
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
--- model/connect.go
+++ model/connect.go
@@ -1,7 +1,28 @@
 package model
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
 // In this package we create the database connection
 
-func Connect() string {
-	return "connected"
+func Connect() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		"localhost", 5432, "postgres", "changemeinprod!", "postgres")
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected!")
+	return db
 }
