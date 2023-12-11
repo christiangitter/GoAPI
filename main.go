f0449f90db4f76a93b1098ad9d0e8173add4c23c
Christian Gitter <christian.gitter@lhind.dlh.de>
2023-12-11 16:41:12 +0100

    add feedback

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"GoAPI/controller"
 	"GoAPI/model"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,6 +12,7 @@ func main() {
 	mux := controller.Register()
 	db := model.Connect()
 	defer db.Close()
+	fmt.Println("Serving on port 3000")
 
 	// log.Fatal is used to log errors and exit the program
 	log.Fatal(http.ListenAndServe(":3000", mux))
